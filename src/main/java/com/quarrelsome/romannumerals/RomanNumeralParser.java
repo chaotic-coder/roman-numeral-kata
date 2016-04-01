7c6d774b4acb4aeeabd8fe96cfa43190b6ab66b3
Satish <satish.kalia@gmail.com>
2016-04-01 14:29:55 +0200

    removed unused code

--- src/main/java/com/quarrelsome/romannumerals/RomanNumeralParser.java
+++ src/main/java/com/quarrelsome/romannumerals/RomanNumeralParser.java
@@ -7,18 +7,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 public class RomanNumeralParser {
 
-    static int calls=0;
-
-    public RomanNumeralParser() {
-        System.out.println("CTR !!!!!!");
-    }
-
     @Autowired
     private  RomanNumeralParserImpl romanNumeralParserImpl;
 
     public String fromInteger(int number) throws RomanNumeralParserOutOfRangeException {
-        calls++;
-        System.out.println("RMP calls: " + calls);
         return romanNumeralParserImpl.parse(number);
     }
 
